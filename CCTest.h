7d97a6d4988d07bc4aafb8c2d12e50b22309f746
Yasuyuki Pham <ypham@Yasuyukis-Mac-mini.local>
2014-04-20 00:26:19 -0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Yasuyuki Pham on 4/20/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Yasuyuki Pham on 4/20/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
